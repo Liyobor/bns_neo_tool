b9adf0ac8ef1770586815f5468558545195818e1
Liyobor <a0963382999@gmail.com>
2025-07-06 02:43:09 +0800

    update powder cal UI

--- powder_cal.js
+++ powder_cal.js
@@ -4,6 +4,11 @@ new Vue({
     data: {
         desertQiPrice: localStorage.getItem('desertQiPrice') || 0,
         moonwaterQiPrice: localStorage.getItem('moonwaterQiPrice') || 0,
+        powderPriceForProfit: localStorage.getItem('powderPriceForProfit') || 0,
+        goldToCrystalRate: localStorage.getItem('goldToCrystalRate') || 0,
+        sellingPrice1: localStorage.getItem('sellingPrice1') || 0,
+        sellingPrice2: localStorage.getItem('sellingPrice2') || 0,
+        sellingPrice3: localStorage.getItem('sellingPrice3') || 0,
     },
     computed: {
         desertPowderPrice: function() {
@@ -11,6 +16,15 @@ new Vue({
         },
         moonwaterPowderPrice: function() {
             return (this.moonwaterQiPrice / 3.4).toFixed(2);
+        },
+        dummyProfit1: function() {
+            return (this.sellingPrice1 - (this.powderPriceForProfit * 10)).toFixed(2);
+        },
+        dummyProfit2: function() {
+            return (this.sellingPrice2 - (this.powderPriceForProfit * 15)).toFixed(2);
+        },
+        dummyProfit3: function() {
+            return (this.sellingPrice3 - (this.powderPriceForProfit * 20)).toFixed(2);
         }
     },
     watch: {
@@ -19,6 +33,21 @@ new Vue({
         },
         moonwaterQiPrice: function(newVal) {
             localStorage.setItem('moonwaterQiPrice', newVal);
+        },
+        powderPriceForProfit: function(newVal) {
+            localStorage.setItem('powderPriceForProfit', newVal);
+        },
+        goldToCrystalRate: function(newVal) {
+            localStorage.setItem('goldToCrystalRate', newVal);
+        },
+        sellingPrice1: function(newVal) {
+            localStorage.setItem('sellingPrice1', newVal);
+        },
+        sellingPrice2: function(newVal) {
+            localStorage.setItem('sellingPrice2', newVal);
+        },
+        sellingPrice3: function(newVal) {
+            localStorage.setItem('sellingPrice3', newVal);
         },
     }
 });
