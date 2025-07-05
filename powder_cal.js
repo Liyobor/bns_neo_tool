65f117d2163b85c4e553580c3003d3f412688840
Liyobor <a0963382999@gmail.com>
2025-07-06 02:29:58 +0800

    update powder cal

--- powder_cal.js
+++ powder_cal.js
@@ -3,15 +3,22 @@ new Vue({
     el: '#powder_cal',
     data: {
         desertQiPrice: localStorage.getItem('desertQiPrice') || 0,
+        moonwaterQiPrice: localStorage.getItem('moonwaterQiPrice') || 0,
     },
     computed: {
-        powderPrice: function() {
+        desertPowderPrice: function() {
             return (this.desertQiPrice / 3.75).toFixed(2);
+        },
+        moonwaterPowderPrice: function() {
+            return (this.moonwaterQiPrice / 3.4).toFixed(2);
         }
     },
     watch: {
         desertQiPrice: function(newVal) {
             localStorage.setItem('desertQiPrice', newVal);
+        },
+        moonwaterQiPrice: function(newVal) {
+            localStorage.setItem('moonwaterQiPrice', newVal);
         },
     }
 });
