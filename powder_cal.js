532c4ff4c2d2a3d0b744a5bfe6c0ac072f98392b
Liyobor <a0963382999@gmail.com>
2025-07-06 02:25:21 +0800

    update powder_cal

--- powder_cal.js
+++ powder_cal.js
@@ -2,27 +2,16 @@
 new Vue({
     el: '#powder_cal',
     data: {
-        itemPrice: localStorage.getItem('itemPrice') || 0,
-        itemSellingPrice: localStorage.getItem('itemSellingPrice') || 0,
+        desertQiPrice: localStorage.getItem('desertQiPrice') || 0,
     },
     computed: {
-        // 在這裡定義您的新計算邏輯
-        // 這只是一個範例，您可以根據您的需求修改
-        totalCost: function() {
-            // 範例成本計算：物品A價格 * 5 + 100
-            return this.itemPrice * 5 + 100;
-        },
-        expectedProfit: function() {
-            // 範例收益計算：(物品B售價 * 0.95) - 總成本
-            return (this.itemSellingPrice * 0.95 - this.totalCost).toFixed(0);
+        powderPrice: function() {
+            return (this.desertQiPrice / 3.75).toFixed(2);
         }
     },
     watch: {
-        itemPrice: function(newVal) {
-            localStorage.setItem('itemPrice', newVal);
-        },
-        itemSellingPrice: function(newVal) {
-            localStorage.setItem('itemSellingPrice', newVal);
+        desertQiPrice: function(newVal) {
+            localStorage.setItem('desertQiPrice', newVal);
         },
     }
 });
